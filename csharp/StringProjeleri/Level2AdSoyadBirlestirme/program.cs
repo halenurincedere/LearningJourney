d8f290c78cedf403edb212958fd16e6737a80f2d
Halenur İncedere <halenurincedere@gmail.com>
2024-12-18 11:27:40 +0300

    Level 2: Ad Soyad Birleştirme Projesi Eklendi

---
+++ csharp/StringProjeleri/Level2AdSoyadBirlestirme/program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Kullanıcı adı al
+        Console.Write("Lütfen kullanıcı adını giriniz:");
+        string? ad = Console.ReadLine();
+        ad = string.IsNullOrWhiteSpace(ad) ? "******" : ad; // Boşsa "Gizli" atar
+
+        // kullanıcı soyadı al
+        Console.Write("Lütfen soyadınızı giriniz:");
+        string? soyad = Console.ReadLine();
+        soyad = string.IsNullOrWhiteSpace(soyad) ? "*****" : soyad; // Boşsa "Gizli" atar
+
+        //Ad soyad birleştir
+        string tamAd = ad + " " + soyad;
+
+        //Ekrana çıktıyı yazdır
+        Console.WriteLine("Giriş Başarılı");
+
+        Console.WriteLine("Hoşgeldin," + tamAd.ToUpper() );
+    }
+}
